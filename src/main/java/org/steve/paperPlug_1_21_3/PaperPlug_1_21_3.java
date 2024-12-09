33322ad4f163823fc2814caf42a4e9f22ac146d4
Steve <stevefaliszewski@gmail.com>
2024-12-08 22:17:41 -0800

    Add DropEvents file for detecting drops

---
+++ src/main/java/org/steve/paperPlug_1_21_3/PaperPlug_1_21_3.java
@@ -0,0 +1,17 @@
+package org.steve.paperPlug_1_21_3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperPlug_1_21_3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/steve/paperPlug_1_21_3/events/DropEvents.java
@@ -0,0 +1,8 @@
+package org.steve.paperPlug_1_21_3.events;
+
+import org.bukkit.event.Listener;
+
+public class DropEvents implements Listener {
+
+
+}
